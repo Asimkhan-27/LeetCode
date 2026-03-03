88fe33b99d0c523291e8571099ea7cb0c38260cb
Muhammad Asim Iftikhar <asimiftikhar84@gmail.com>
2026-03-03 13:27:05 +0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (29.10%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -4,13 +4,14 @@ class Solution:
         result = []
         
         for i in range(numRows):
-            sr = []
-            for j in range(i+1):
-                sr.append(math.comb(i, j))
-            result.append(sr)
-                
-                    
-                    
+            row = [1] * (i + 1)
+            
+            # Compute inner elements using previous row
+            for j in range(1, i):
+                row[j] = result[i-1][j-1] + result[i-1][j]
+            
+            result.append(row)
+        
         return result
 
 
